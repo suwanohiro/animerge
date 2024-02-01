13ab1a53553bd17ea7dfafa38bc254e993c33ef4
諏訪野ヒロ <62730538+suwanohiro@users.noreply.github.com>
2024-02-01 18:41:42 +0900

    未使用usingを削除

--- Assets/Script/OnlineRanking/Receive/RankingManager.cs
+++ Assets/Script/OnlineRanking/Receive/RankingManager.cs
@@ -1,11 +1,5 @@
-using System;
 using System.Collections;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.PackageManager;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class RankingManager : MonoBehaviour
 {
