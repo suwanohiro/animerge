1ef19e71ba9e8ab01aa0ab4cd1dc26bd64f5ed21
lemon <rikulemon1105@gmail.com>
2024-02-02 02:26:22 +0900

    ResultのScore調整

---
+++ Assets/Script/Score.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.Build.Content;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI Text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text.SetText($"Score:345");
+        //Text.SetText($"Score:{GameManager.Score.ToString()}");
+        //Text.text += GameManager.Score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
