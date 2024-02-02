ca4535ca81e5e5d6f65b0e7edd28ea06895e471d
suwanohiro <supermariomaker1110@gmail.com>
2024-02-02 10:34:22 +0900

    スコア表示機能修正

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameMnager : MonoBehaviour
 {
     [SerializeField]
-    UnityEngine.UI.Text scoreText;
+    TextMeshProUGUI scoreText;
 
 	// Start is called before the first frame update
 	void Start()
@@ -19,6 +20,6 @@ public class GameMnager : MonoBehaviour
 
         int score = ScoreCount.GetScore();
 
-		scoreText.text = score.ToString();
+		scoreText.text = score.ToString("N0");
     }
 }
